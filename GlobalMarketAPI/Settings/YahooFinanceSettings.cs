96ce1cdbc35a8d4403ec1df694e7ecf6033718b8
nirzaf <mfmfazrin1986@gmail.com>
2022-06-07 15:24:02 +0530

    pick it

--- GlobalMarketAPI/Settings/YahooFinanceSettings.cs
+++ GlobalMarketAPI/Settings/YahooFinanceSettings.cs
@@ -4,10 +4,6 @@
     {
         public string? APIKey { get; set; }
         public string? BaseURL { get; set; }
-        public Dictionary<string, string>? SymbolMapInfo
-        {
-            get;
-            set;
-        }
+        public Dictionary<string, string>? SymbolMapInfo  { get; set; }
     }
 }
